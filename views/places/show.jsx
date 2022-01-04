a9b018e0774758b99a8107dc3cb155b5a03104c3
KevinsCodeStorage <89494039+KevinsCodeStorage@users.noreply.github.com>
2022-01-03 20:25:23 -0500

    delete functionality added

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -11,6 +11,16 @@ function show (data) {
             <p>currently unrated</p>
             <h2>Comments</h2>
             <p>No comments yet!</p>
+            <div>
+                <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                    Edit
+                </a>       
+                <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                    <button type="submit" className="btn btn-danger">
+                        Delete
+                    </button>
+                </form>    
+            </div>
           </main>
         </Def>
     )
