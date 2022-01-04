f64f7f93b2d277f17358ec762c21c5fbec6e6538
KevinsCodeStorage <89494039+KevinsCodeStorage@users.noreply.github.com>
2022-01-03 20:05:49 -0500

    show route fix

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,16 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{ data.place.name }</h1>
+            <img src={data.place.pic} alt={data.place.name} width="400" height="400"/>
+            <h2>Ratings</h2>
+            <p>currently unrated</p>
+            <h2>Comments</h2>
+            <p>No comments yet!</p>
           </main>
         </Def>
     )
